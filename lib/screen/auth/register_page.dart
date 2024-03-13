1c4e45ce94f7eef5eee09eb670f76b9349c359a5
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-03-13 18:38:33 +0700

    feat: custom color and some changes

--- lib/screen/auth/register_page.dart
+++ lib/screen/auth/register_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -6,10 +8,55 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.asset(
+                  'assets/images/image-1.jpg',
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Register'.toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                        shadows: [
+                          Shadow(
+                            color: Colors.white,
+                            blurRadius: 20,
+                          ),
+                          Shadow(
+                            color: Colors.black,
+                            blurRadius: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text('Register for VacayTix Today'),
+                  ],
+                ),
+              ],
+            ),
+            SafeArea(
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const Color primaryColor = Color.fromARGB(255, 0, 0, 25); // deep blue
-  static const Color secondaryColor =
-      Color.fromARGB(255, 255, 255, 255); // white
-  static const Color tertiaryColor = Color.fromARGB(255, 0, 0, 0); // black
-  static const Color accentColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color primaryColor = Color(0xFF83424B);
+  static const Color secondaryColor = Color(0xFF471F34);
+  static const Color tertiaryColor = Color(0xFFE1C9C2);
+  static const Color accentColor = Color(0xFF987971);
+  static const Color accentColor2 = Color(0xFFE6857A);
 }
