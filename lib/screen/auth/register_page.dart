0a3a73210012ac4e6975a8760b03d8ebf394d9da
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-03-13 13:42:51 +0700

    feat:just changes

---
+++ lib/screen/auth/register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primaryColor = Color.fromARGB(255, 0, 0, 25); // deep blue
+  static const Color secondaryColor =
+      Color.fromARGB(255, 255, 255, 255); // white
+  static const Color tertiaryColor = Color.fromARGB(255, 0, 0, 0); // black
+  static const Color accentColor = Color.fromARGB(255, 255, 255, 255);
+}
